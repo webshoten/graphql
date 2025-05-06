d64e469f1c7abc4f483b7aa28a2a7242fdf47f8b
webshoten <weboshimashoten@gmail.com>
2025-05-06 23:34:19 +0900

    biome

--- vite.plugins/schema-watcher.ts
+++ vite.plugins/schema-watcher.ts
@@ -1,22 +1,28 @@
-import { Plugin } from 'vite';
-import { spawn } from 'child_process';
-import path from 'path';
+import { spawn } from "node:child_process";
+import path from "node:path";
+import type { Plugin } from "vite";
 
 export default function schemaWatcherPlugin(): Plugin {
-  return {
-    name: 'custom-watcher-plugin',
-    handleHotUpdate({ file }) {
-      const normalizedPath = file.replace(/\\/g, '/'); // Windows対策でパスを正規化
+	return {
+		name: "custom-watcher-plugin",
+		handleHotUpdate({ file }) {
+			const normalizedPath = file.replace(/\\/g, "/"); // Windows対策でパスを正規化
 
-      if (normalizedPath.startsWith(path.resolve('app/graphql').replace(/\\/g, '/'))) {
-        console.log(`📄 Detected change in ${normalizedPath}, running script...`);
+			if (
+				normalizedPath.startsWith(
+					path.resolve("app/graphql").replace(/\\/g, "/"),
+				)
+			) {
+				console.log(
+					`📄 Detected change in ${normalizedPath}, running script...`,
+				);
 
-        const child = spawn('npm', ['run', 'codegen'], { stdio: 'inherit' });
+				const child = spawn("npm", ["run", "codegen"], { stdio: "inherit" });
 
-        child.on('close', (code) => {
-          console.log(`✅ Script exited with code ${code}`);
-        });
-      }
-    },
-  };
+				child.on("close", (code) => {
+					console.log(`✅ Script exited with code ${code}`);
+				});
+			}
+		},
+	};
 }
